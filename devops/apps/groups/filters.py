4a97d82d288409324d5d6b46efe6857463730ca1
sealandsigh <zhujiajunzhu@qq.com>
2020-12-22 15:47:28 +0800

    group用户组配合前端功能完成

--- devops/apps/groups/filters.py
+++ devops/apps/groups/filters.py
@@ -6,8 +6,13 @@ import django_filters
 from django.contrib.auth.models import Group
 
 class GroupFilter(django_filters.FilterSet):
+    """
+    group 搜索过滤类
+    """
     name = django_filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
         model = Group
         fields = ["name"]
+        # 原代码中是元组, 这里用列表貌似也没问题。
+        # fields = (name,)

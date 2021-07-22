0cecfc172d4f0849ce7b28f1860c9823f37bf1a9
sealandsigh <zhujiajunzhu@qq.com>
2021-07-22 15:04:22 +0800

    tasks通过share_task进行设置,并且更改存储result位置

--- devops/apps/celery-test/tasks.py
+++ devops/apps/celery-test/tasks.py
@@ -5,16 +5,19 @@
 import time
 from celery import Celery
 from devops import celeryconfig
-from devops.celery import app
+# from devops.celery import app
+from celery import shared_task
 
 # celery_app = Celery('tasks', backend='redis://localhost:6379', broker='redis://localhost:6379')
-celery_app = app
+# celery_app = app
+
 
 # celery_app.config_from_object(celeryconfig)
 # celery_app.conf.task_default_queue = 'work_queue'
 
 # this is a function about need many time
-@celery_app.task()
+# @celery_app.task()
+@shared_task()
 def add(a, b):
     time.sleep(5)
     return a + b

3165332f98131ac78796f4350dba3cf8fce1e1dc
sealandsigh <zhujiajunzhu@qq.com>
2021-08-02 11:34:10 +0800

    esmanager模型调整增加用户名密码字段

--- devops/apps/esmanager/models.py
+++ devops/apps/esmanager/models.py
@@ -3,6 +3,7 @@
 # DATE:2021/7/29
 
 from django.db import models
+import random
 
 class Escluster(models.Model):
     name = models.CharField("集群名称", max_length=50, help_text="集群名称")
@@ -13,6 +14,22 @@ class Escluster(models.Model):
     port = models.IntegerField("集群端口", default=9200, help_text="集群端口")
     monitorPort = models.IntegerField("集群端口", default=9109, help_text="集群端口")
     saveDay = models.CharField("索引默认保留天数", max_length=32, help_text="索引默认保留天数")
+    username = models.CharField("集群用户", max_length=50, help_text="集群用户", null=True, blank=True)
+    password = models.CharField("集群密码", max_length=50, help_text="集群密码", null=True, blank=True)
+
+    def __str__(self):
+        return self.code
+
+    def save(self, *args, **kwargs):
+        salt = random.sample('zyxwvutsrqponmlkjihgfedcba', 5)
+        result = ''.join(salt)
+        self.password = self.password + result
+        super(Escluster, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = '集群'
+        verbose_name_plural = verbose_name
+
 
 
 class Indexmanage(models.Model):
@@ -22,5 +39,3 @@ class Indexmanage(models.Model):
     createTime = models.DateTimeField("创建时间", auto_now_add=True, help_text="创建时间")
     updateTime = models.DateTimeField("更新时间", auto_now=True, help_text="更新时间")
     monitorSt = models.BooleanField("监控状态", help_text="监控状态")
-
-
